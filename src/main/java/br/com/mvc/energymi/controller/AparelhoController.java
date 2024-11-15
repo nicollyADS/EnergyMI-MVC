2cc2bb1100ce7f2bd7ef10468f6b1080c793c630
nicollyADS <adm.nicolly@gmail.com>
2024-11-15 20:44:47 -0300

    pesquisar

--- src/main/java/br/com/mvc/energymi/controller/AparelhoController.java
+++ src/main/java/br/com/mvc/energymi/controller/AparelhoController.java
@@ -10,12 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -99,5 +97,12 @@ public class AparelhoController {
         aparelhoRepository.deleteById(id);
         redirectAttributes.addFlashAttribute("mensagem", "aparelho removido com sucesso");
         return "redirect:/aparelho/listar";
+    }
+
+    @GetMapping("pesquisar")
+    public String pesquisarAparelhos(@RequestParam String query, Model model) {
+        List<Aparelho> aparelhos = aparelhoRepository.findByNomeContainingIgnoreCase(query);
+        model.addAttribute("aparelhos", aparelhos);
+        return "aparelho/pesquisar";
     }
 }
--- src/main/java/br/com/mvc/energymi/repository/AparelhoRepository.java
+++ src/main/java/br/com/mvc/energymi/repository/AparelhoRepository.java
@@ -3,5 +3,9 @@ package br.com.mvc.energymi.repository;
 import br.com.mvc.energymi.model.Aparelho;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AparelhoRepository extends JpaRepository<Aparelho, Long> {
+    List<Aparelho> findByNomeContainingIgnoreCase(String nome);
+
 }
