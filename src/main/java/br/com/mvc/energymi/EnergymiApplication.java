f4d36d59b6aad3790ecb93d14e5f772ce78ec69b
nicollyADS <adm.nicolly@gmail.com>
2024-11-15 12:07:46 -0300

    commit inicial

---
+++ src/main/java/br/com/mvc/energymi/EnergymiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mvc.energymi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergymiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergymiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mvc/energymi/EnergymiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mvc.energymi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergymiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
